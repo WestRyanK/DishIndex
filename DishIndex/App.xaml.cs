b1a28bc1a8f5f02a6a3477afedd3b07248e10b50
Ryan West <west.ryan.k@gmail.com>
2024-01-02 15:28:36 -0700

    Initial Commit

---
+++ DishIndex/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DishIndex
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DishIndex/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DishIndex
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
