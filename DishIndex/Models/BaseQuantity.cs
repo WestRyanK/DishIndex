d41e01ae33282838050a3ad3ca288b734a024ed4
Ryan West <west.ryan.k@gmail.com>
2024-01-02 20:40:05 -0700

    Make generic base quantity

---
+++ DishIndex/Models/BaseQuantity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DishIndex.Models;
+
+internal enum SystemOfMeasurement
+{
+	None,
+	Metric,
+	US
+}
+
+internal abstract class BaseQuantity<Q, U>
+	where Q : BaseQuantity<Q, U>, new()
+	where U : struct, Enum
+{
+	public double Scalar { get; set; }
+	public U Unit { get; set; }
+
+	public Q ConvertTo(U unit)
+	{
+		double milliliters = Scalar * UnitQuantity(Unit);
+		double inNewUnit = milliliters / UnitQuantity(unit);
+		return new Q() { Scalar = inNewUnit, Unit = unit };
+	}
+
+	public abstract double UnitQuantity(U unit);
+
+	public static Q operator *(BaseQuantity<Q, U> quantity, double scale) => new Q() { Scalar = quantity.Scalar * scale, Unit = quantity.Unit };
+}
