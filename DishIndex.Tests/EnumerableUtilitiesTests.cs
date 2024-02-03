3f1374d0b12978da9209f0c3733f471e79d7b4f4
Ryan West <west.ryan.k@gmail.com>
2024-02-02 23:55:55 -0700

    Add IndexOf extension

---
+++ DishIndex.Tests/EnumerableUtilitiesTests.cs
@@ -0,0 +1,55 @@
+﻿using DishIndex.Utilities;
+
+namespace DishIndex.Tests;
+
+public class EnumerableUtilitiesTests
+{
+	[Fact]
+	public void TestIndexOf()
+	{
+		List<int> list = Enumerable.Range(1, 10).ToList();
+
+		Assert.Equal(2, list.IndexOf(x => x == 3));
+		Assert.Equal(2, list.IndexOf(x => x % 3 == 0));
+	}
+
+	[Fact]
+	public void TestIndexOf_None()
+	{
+		List<int> list = Enumerable.Range(1, 10).ToList();
+
+		Assert.Equal(-1, list.IndexOf(x => x == 12));
+	}
+
+	[Fact]
+	public void TestIndexOf_Empty()
+	{
+		List<int> list = Enumerable.Empty<int>().ToList();
+
+		Assert.Equal(-1, list.IndexOf(x => x == 12));
+	}
+
+	[Fact]
+	public void TestIndexOfWithIndex()
+	{
+		List<int> list = Enumerable.Range(1, 10).ToList();
+
+		Assert.Equal(3, list.IndexOf((i, x) => i + x == 7));
+	}
+
+	[Fact]
+	public void TestIndexOfWithIndex_None()
+	{
+		List<int> list = Enumerable.Range(1, 10).ToList();
+
+		Assert.Equal(-1, list.IndexOf((i, x) => i + x == 40));
+	}
+
+	[Fact]
+	public void TestIndexOfWithIndex_Empty()
+	{
+		List<int> list = Enumerable.Empty<int>().ToList();
+
+		Assert.Equal(-1, list.IndexOf((i, x) => i + x == 12));
+	}
+}
---
+++ DishIndex/Utilities/EnumerableUtilities.cs
@@ -0,0 +1,34 @@
+﻿namespace DishIndex.Utilities;
+
+internal static class EnumerableUtilities
+{
+	public static int IndexOf<T>(this IEnumerable<T> values, Func<T, bool> predicate)
+	{
+		int i = 0;
+		IEnumerator<T> enumerator = values.GetEnumerator();
+		while (enumerator.MoveNext())
+		{
+			if (predicate(enumerator.Current))
+			{
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+
+	public static int IndexOf<T>(this IEnumerable<T> values, Func<int, T, bool> predicate)
+	{
+		int i = 0;
+		IEnumerator<T> enumerator = values.GetEnumerator();
+		while (enumerator.MoveNext())
+		{
+			if (predicate(i, enumerator.Current))
+			{
+				return i;
+			}
+			i++;
+		}
+		return -1;
+	}
+}
