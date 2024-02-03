8605efe09635f5798c624445e6f1f042d29f011d
Ryan West <west.ryan.k@gmail.com>
2024-02-03 00:43:50 -0700

    Add initial RecipeParser to split sections

---
+++ DishIndex.Tests/RecipeParserTests.cs
@@ -0,0 +1,85 @@
+﻿using DishIndex.Parsers;
+using System.Runtime.CompilerServices;
+
+namespace DishIndex.Tests;
+
+public class RecipeParserTests
+{
+	private static readonly IList<string> RecipeNoTipsLines = [
+		"0: Recipe Name",
+		"1: Garbage 1",
+		"2: Garbage 2",
+		"3: An Ingredients Title",
+		"4: Ingredient 1",
+		"5: Ingredient 2",
+		"6: Ingredient 3",
+		"7: Ingredient 4",
+		"8: Instructions Start Here",
+		"9: Instruction 1",
+		"10: Instruction 2",
+	];
+
+	private static readonly IList<string> TipsLines = [
+		"11: Tips",
+		"12: Tip #1",
+		"13: Tip #2",
+		"14: Tip #3",
+	];
+
+	private static readonly IList<string> RecipeWithTipsLines = [.. RecipeNoTipsLines, .. TipsLines];
+
+
+	private void TestSection(IEnumerable<string> result, int expectedCount, int expectedFirst, int expectedLast)
+	{
+		Assert.Equal(expectedCount, result.Count());
+		Assert.Equal(expectedFirst.ToString(), result.First());
+		Assert.Equal(expectedLast.ToString(), result.Last());
+	}
+
+	[Fact]
+	public void TestGetSection()
+	{
+		IList<string> input = Enumerable.Range(0, 10).Select(x => x.ToString()).ToList();
+		TestSection(RecipeParser.GetSection(input, 0, 10), 10, 0, 9);
+		TestSection(RecipeParser.GetSection(input, 1, 5), 4, 1, 4);
+	}
+
+	private void TestRecipeSection(IEnumerable<string> actual, int expectedCount, int expectedFirstIndex, int expectedLastIndex)
+	{
+		Assert.Equal(expectedCount, actual.Count());
+		Assert.Equal(RecipeWithTipsLines[expectedFirstIndex], actual.First());
+		Assert.Equal(RecipeWithTipsLines[expectedLastIndex], actual.Last());
+	}
+
+	[Fact]
+	public void TestGetRecipeSections()
+	{
+		RecipeParser.GetRecipeSections(
+			RecipeWithTipsLines,
+			out string recipeName,
+			out IEnumerable<string> ingredientsSection,
+			out IEnumerable<string> instructionsSection,
+			out IEnumerable<string>? tipsSection);
+
+		Assert.Equal(RecipeWithTipsLines[0], recipeName);
+		TestRecipeSection(ingredientsSection, 5, 3, 7);
+		TestRecipeSection(instructionsSection, 3, 8, 10);
+		TestRecipeSection(tipsSection!, 4, 11, 14);
+	}
+
+	[Fact]
+	public void TestGetRecipeSections_NoTips()
+	{
+		RecipeParser.GetRecipeSections(
+			RecipeNoTipsLines,
+			out string recipeName,
+			out IEnumerable<string> ingredientsSection,
+			out IEnumerable<string> instructionsSection,
+			out IEnumerable<string>? tipsSection);
+
+		Assert.Equal(RecipeWithTipsLines[0], recipeName);
+		TestRecipeSection(ingredientsSection, 5, 3, 7);
+		TestRecipeSection(instructionsSection, 3, 8, 10);
+		Assert.Null(tipsSection);
+	}
+}
---
+++ DishIndex/Parsers/RecipeParser.cs
@@ -0,0 +1,62 @@
+﻿using DishIndex.Models;
+using DishIndex.Utilities;
+using System.Linq;
+
+namespace DishIndex.Parsers;
+
+internal class RecipeParser
+{
+	internal const string IngredientHeader = "Ingredient";
+	internal const string InstructionHeader = "Instruction";
+	internal const string TipHeader = "Tip";
+	public static Recipe Parse(string recipeText)
+	{
+		string[] recipeLines = recipeText.Split(["\r\n", "\r", "\n"], StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+		if (recipeLines.Length <= 3)
+		{
+			throw new FormatException("Empty Recipe");
+		}
+
+		GetRecipeSections(
+			recipeLines,
+			out string recipeName,
+			out IEnumerable<string> ingredientsSection,
+			out IEnumerable<string> instructionsSection,
+			out IEnumerable<string>? tipsSection);
+
+
+		return null;
+	}
+
+	internal static void GetRecipeSections(IList<string> recipeLines,
+		out string recipeName,
+		out IEnumerable<string> ingredientsSection,
+		out IEnumerable<string> instructionsSection,
+		out IEnumerable<string>? tipsSection)
+	{
+		recipeName = recipeLines[0];
+
+		int ingredientsStartIndex = recipeLines.IndexOf((i, line) => i > 0 && line.Contains(IngredientHeader));
+		if (ingredientsStartIndex < 0)
+		{
+			throw new FormatException($"Recipe missing '{IngredientHeader}' header.");
+		}
+
+		int instructionsStartIndex = recipeLines.IndexOf((i, line) => i > ingredientsStartIndex && line.Contains(InstructionHeader));
+		if (instructionsStartIndex < 0)
+		{
+			throw new FormatException($"Recipe missing '{InstructionHeader}' header.");
+		}
+
+		int tipsStartIndex = recipeLines.IndexOf((i, line) => i > instructionsStartIndex && line.Contains(TipHeader));
+
+		ingredientsSection = GetSection(recipeLines, ingredientsStartIndex, instructionsStartIndex);
+		instructionsSection = GetSection(recipeLines, instructionsStartIndex, tipsStartIndex > 0 ? tipsStartIndex : recipeLines.Count);
+		tipsSection = (tipsStartIndex > 0) ?  GetSection(recipeLines, tipsStartIndex, recipeLines.Count) : null;
+	}
+
+	internal static IEnumerable<string> GetSection(IList<string> recipeLines, int startIndex, int endIndex)
+	{
+		return recipeLines.Skip(startIndex).Take(endIndex - startIndex);
+	}
+}
