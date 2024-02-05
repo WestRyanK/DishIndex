9546d601293210f61aab249994337627902b1c16
Ryan West <west.ryan.k@gmail.com>
2024-02-04 23:20:24 -0700

    Fetch OpenAiApiKey and test it is found

---
+++ DishIndex.Core/Utilities/Secrets.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace DishIndex.Core.Utilities;
+
+internal class Secrets
+{
+
+	private static readonly IConfigurationRoot _config = new ConfigurationBuilder()
+		.AddUserSecrets(Assembly.GetExecutingAssembly())
+		.Build();
+
+	private const string OpenAiApiKeyName = "OpenAiApiKey";
+	public static string OpenAiApiKey => _config[OpenAiApiKeyName];
+}
---
+++ DishIndex.Tests/SecretsTest.cs
@@ -0,0 +1,17 @@
+﻿using DishIndex.Core.Utilities;
+using System.Text.RegularExpressions;
+
+namespace DishIndex.Tests;
+
+public class SecretsTest
+{
+	[Fact]
+	public void OpenAiApiKey_Test()
+	{
+		string apiKey = Secrets.OpenAiApiKey;
+		Assert.NotNull(apiKey);
+#pragma warning disable xUnit2008 // Do not use boolean check to match on regular expressions
+		Assert.True(Regex.IsMatch(apiKey, @"^sk-\w{48}$"), "API key doesn't match expected regular expression.");
+#pragma warning restore xUnit2008 // Do not use boolean check to match on regular expressions
+	}
+}
